52662d93ca1eefd651c7032756e04398ab708f95
21toffy <oketofoke@gmail.com>
2022-10-27 16:12:13 +0100

    white list request urls

--- middleware/authMiiddleware.go
+++ middleware/authMiiddleware.go
@@ -9,9 +9,21 @@ import (
 
 func JwtAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		var IPWhitelist = map[string]bool{
+			"127.0.0.1": true,
+		}
+
+		ip := c.ClientIP()
+		if !IPWhitelist[ip] {
+			c.IndentedJSON(http.StatusForbidden, gin.H{
+				"error": "You are not authorised to use this endpoint",
+			})
+			return
+		}
 		err := helpers.TokenValid(c)
 		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 			c.Abort()
 			return
 		}
